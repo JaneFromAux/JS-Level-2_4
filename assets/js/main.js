ff1da8fb3ba9a5fa721fe9ee3883b8544f1ebdc8
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-05-11 20:55:42 +0200

    fertig

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+
+
+
+function sumSum(x, y) {
+    if (x == y) {
+        console.log((x + y) * 5);
+    }
+    else {
+        console.log(x + y);
+    }
+
+}
+
+sumSum(5, 7);
+sumSum(17, 30);
+sumSum(8, 8);
